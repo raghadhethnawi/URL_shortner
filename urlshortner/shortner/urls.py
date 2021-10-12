befb9159b9c1bc3c60d1c7b234424575e9358715
raghadhethnawi <raghadhethnawi@gmail.com>
2021-10-12 13:22:11 +0300

    'shortner'

--- urlshortner/shortner/urls.py
+++ urlshortner/shortner/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('create', views.create, name='create'),
+    path('<str:pk>', views.go, name='go'),
 ]
